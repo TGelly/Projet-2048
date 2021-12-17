c892176d5afccc619d618c38321d18119f628fce
TGelly <gellythibault@yahoo.fr>
2021-12-17 14:48:15 +0100

    correction typos: ÇA COMPILE

--- rejoue.c
+++ rejoue.c
@@ -21,13 +21,13 @@ void Rejoue(){
 
     //On ouvre le dossier 2048 et on propose à l'utilisateur d'ouvrir un des fichiers textes qu'il contient.
     DIR* FD;
-    if (NULL == (FD = opendir ("/home/Documents/VSCode/L2/Prog\ C/2048"))){
+    if (NULL == (FD = opendir ("/gamefiles"))){
         printf("Erreur: Echec de l'ouverture du dossier.\n");
     }
 
     //On ouvre le fichier choisi par l'utilisateur
-    char cheminFichier;
-    FILE* fichier = fopen(cheminFichier, 'r');
+    char* cheminFichier;
+    FILE* fichier = fopen(cheminFichier, "r");
 
     //On rejoue la partie en l'affichant
     rejoueInitialisation(fichier);
